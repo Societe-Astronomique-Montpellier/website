39f62338bece85bcdad27336b036a014f42d2129
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-07-10 17:20:47 +0200

    Block testimonial

--- components/home/BlockTestimonial.vue
+++ components/home/BlockTestimonial.vue
@@ -12,51 +12,59 @@ const { block } = toRefs(props)
 
 <template>
   <!--Background-->
-  <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl " v-if="block">
-    <h3 class="mb-6 text-3xl font-bold">{{ block.data.title }}</h3>
-    <div
-        class="mb-6 pb-2 text-neutral-600 dark:text-neutral-300 md:mb-12 md:pb-0">
-      <prismic-rich-text :field="block.data.content" />
-    </div>
-  </div>
+  <section
+      class="rounded-md p-6 text-center shadow-lg md:p-12 md:text-left bg-gray-600"
+  >
+      <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl " v-if="block">
+        <h3 class="mb-6 text-3xl font-bold">{{ block.data.title }}</h3>
+        <div
+            class="mb-6 pb-2 text-neutral-600 dark:text-neutral-300 md:mb-12 md:pb-0">
+          <prismic-rich-text :field="block.data.content" />
+        </div>
+      </div>
 
-  <div class="grid gap-6 text-center md:grid-cols-3 lg:gap-12 bg-gray-600">
-    <div class="mb-12 md:mb-0">
-      <div class="mb-6 flex justify-center">
-<!--        <prismic-image-->
-<!--          :field="block.data.testimonial_vignette_1"-->
-<!--          class="w-32 rounded-full shadow-lg dark:shadow-black/30"-->
-<!--        />-->
+      <div class="grid gap-6 text-center md:grid-cols-3 lg:gap-12 ">
+        <div class="mb-12 md:mb-0">
+          <div class="mb-6 flex justify-center">
+            <prismic-image
+              :field="block.data.testimonial_vignette_1"
+              class="w-32 rounded-full shadow-lg dark:shadow-black/30"
+            />
+          </div>
+          <p class="mb-4 italic text-neutral-600 dark:text-neutral-300">
+            {{ block.data.testimonial_text_1 }}
+          </p>
+        </div>
+        <div class="mb-12 md:mb-0">
+          <div class="mb-6 flex justify-center">
+            <!--        <prismic-image-->
+            <!--          :field="block.data.testimonial_vignette_3"-->
+            <!--          class="w-32 rounded-full shadow-lg dark:shadow-black/30"-->
+            <!--        />-->
+          </div>
+          <p class="mb-4 italic text-neutral-600 dark:text-neutral-300">
+            <!--        <prismic-text :field="block.data.testimonial_text_2" />-->
+            {{ block.data.testimonial_text_2 }}
+          </p>
+        </div>
+        <div class="mb-0">
+          <div class="mb-6 flex justify-center">
+            <!--        <prismic-image-->
+            <!--          :field="block.data.testimonial_vignette_3"-->
+            <!--          class="w-32 rounded-full shadow-lg dark:shadow-black/30"-->
+            <!--        />-->
+          </div>
+          <p class="mb-4 italic text-neutral-600 dark:text-neutral-300">
+            <!--        <prismic-text :field="block.data.testimonial_text_3" />-->
+            {{ block.data.testimonial_text_3 }}
+          </p>
+        </div>
       </div>
-      <p class="mb-4 text-neutral-600 dark:text-neutral-300">
-        {{ block.data.testimonial_text_1 }}
-      </p>
-    </div>
-    <div class="mb-12 md:mb-0">
-      <div class="mb-6 flex justify-center">
-<!--        <prismic-image-->
-<!--          :field="block.data.testimonial_vignette_3"-->
-<!--          class="w-32 rounded-full shadow-lg dark:shadow-black/30"-->
-<!--        />-->
-      </div>
-      <p class="mb-4 text-neutral-600 dark:text-neutral-300">
-<!--        <prismic-text :field="block.data.testimonial_text_2" />-->
-        {{ block.data.testimonial_text_2 }}
-      </p>
-    </div>
-    <div class="mb-0">
-      <div class="mb-6 flex justify-center">
-<!--        <prismic-image-->
-<!--          :field="block.data.testimonial_vignette_3"-->
-<!--          class="w-32 rounded-full shadow-lg dark:shadow-black/30"-->
-<!--        />-->
-      </div>
-      <p class="mb-4 text-neutral-600 dark:text-neutral-300">
-<!--        <prismic-text :field="block.data.testimonial_text_3" />-->
-        {{ block.data.testimonial_text_3 }}
-      </p>
-    </div>
-  </div>
+  </section>
+
+
+
+
 </template>
 
 <style scoped>

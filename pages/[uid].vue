3aa0d160c83402dcb90bf1d730ec0280d83cb7b0
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-08-19 15:01:32 +0200

    Test SEO

--- pages/[uid].vue
+++ pages/[uid].vue
@@ -34,7 +34,7 @@ import { useBannerImage } from "@/composables/useBannerImage";
 
 const { uid } = route.params as { uid: string }
 
-const { data: page_thematique, error} = useAsyncData(
+const { data, error} = useAsyncData(
   uid,
   async () => {
     const response = await prismic.client.getByUID<PageThematiqueDocument>('page_thematique', uid)
@@ -50,7 +50,7 @@ const { data: page_thematique, error} = useAsyncData(
     }) as PageArticleDocument[]
 
     return {
-      thematic: response,
+      page_thematic: response,
       publication_date: response.last_publication_date ?? response.first_publication_date,
       articles: articles
     };
@@ -60,22 +60,22 @@ const { data: page_thematique, error} = useAsyncData(
 const knowMoreLabel = computed<string>(() => t('layout.knowMore'))
 const shareSocialMedia = useSocialShareMedia();
 const richTextSerializer = useRichTextSerializer();
-const formatedDate = useState('formatedDate', () => useFormatIntoFrenchDate(page_thematique.value?.publication_date, 'short'));
+const formatedDate = useState('formatedDate', () => useFormatIntoFrenchDate(data.value?.publication_date, 'short'));
 
-const imageBanner = computed<ImageField | FilledImageFieldImage | EmptyImageFieldImage | undefined>(() => useBannerImage(page_thematique.value?.thematic.data.image_banner, isMobile))
-const metaTitle: ComputedRef<string> = computed<string>(() => !isFilled.keyText(page_thematique.value?.thematic.data.meta_title) ? `${page_thematique.value?.thematic.data.meta_title}` : `${page_thematique.value?.thematic.data.title}`);
-const metaDescription: ComputedRef<string> = computed<string>(() => `${page_thematique.value?.thematic.data.meta_description}`);
+const imageBanner = computed<ImageField | FilledImageFieldImage | EmptyImageFieldImage | undefined>(() => useBannerImage(data.value?.page_thematic.data.image_banner, isMobile))
+const metaTitle = computed<string>(() => !isFilled.keyText(data.value?.page_thematic.data.meta_title) ? `${data.value?.page_thematic.data.meta_title}` : `${data.value?.page_thematic.data.title}`);
+const metaDescription = computed<string>(() => `${data.value?.page_thematic.data.meta_description}`);
 
 useSeo({
   title: metaTitle.value,
   description: metaDescription.value,
-  image: page_thematique.value?.thematic.data.image_vignette.vignette,
+  image: data.value?.page_thematic.data.image_vignette.vignette,
   imageAlt: null
 })
 </script>
 
 <template>
-  <section v-if="page_thematique">
+  <section v-if="data">
     <div class="max-w-screen-xl w-full mx-auto relative  mb-2"> <!-- max-w-screen-lg -->
       <HeaderPage :image="imageBanner" />
       <div class="max-w-screen-md mx-auto">
@@ -83,9 +83,9 @@ useSeo({
           class="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal"
         >
           <div class="bg-white relative top-0 -mt-32 p-5 sm:p-10" >
-            <Breadcrumbs :listIds="[page_thematique.thematic.id]" :currentUid="page_thematique.thematic.uid" />
-            <h1 class="text-gray-900 font-bold text-4xl mb-2 font-raleway">{{ page_thematique.thematic.data.title }}</h1>
-            <h2 class="text-gray-900 font-semibold text-2xl mb-2 leading-normal">{{ page_thematique.thematic.data.subtitle }}</h2>
+            <Breadcrumbs :listIds="[data.page_thematic.id]" :currentUid="data.page_thematic.uid" />
+            <h1 class="text-gray-900 font-bold text-4xl mb-2 font-raleway">{{ data.page_thematic.data.title }}</h1>
+            <h2 class="text-gray-900 font-semibold text-2xl mb-2 leading-normal">{{ data.page_thematic.data.subtitle }}</h2>
 
             <Icon name="material-symbols:arrow-right-alt" v-show="false" />
             <div :class="(isMobile) ? `my-4 grid gap-4 px-1`: `my-8 grid grid-cols-[50px_1fr] gap-4 px-2`">
@@ -99,13 +99,13 @@ useSeo({
               </div>
               <div data-content>
                 <prismic-rich-text
-                  :field="page_thematique.thematic.data.content"
+                  :field="data.page_thematic.data.content"
                   :serializer="richTextSerializer"
                 />
 
                 <div class="text-gray-700 text-xs mt-5">
                   <span id="span_author" class="font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    {{ $t('page.author')}} {{ page_thematique?.thematic.data.author }}
+                    {{ $t('page.author')}} {{ data.page_thematic.data.author }}
                   </span> le
                     <span id="span_date" class="font-medium hover:text-gray-900 transition duration-500 ease-in-out" v-if="formatedDate">
                     {{ formatedDate }}
@@ -120,10 +120,10 @@ useSeo({
       </div>
 
       <BlockListCards
-        v-if="0 < page_thematique.articles.length"
+        v-if="0 < data.articles.length"
         :title-block="knowMoreLabel"
-        :items="page_thematique.articles"
-        :parentItem="page_thematique.thematic"
+        :items="data.articles"
+        :parentItem="data.page_thematic"
       />
     </div>
   </section>

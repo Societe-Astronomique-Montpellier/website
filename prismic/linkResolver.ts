676220d7cc83b7e1a6d3b599f6ed169a806df617
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-08-23 17:22:52 +0200

    link resolver

--- prismic/linkResolver.ts
+++ prismic/linkResolver.ts
@@ -2,18 +2,14 @@ import {type LinkResolverFunction} from '@prismicio/client';
 import type {FilledContentRelationshipField} from "@prismicio/client/src/types/value/contentRelationship";
 
 const linkResolver: LinkResolverFunction = (document: any): string|null => {
-    if ('homepage' === document.type) {
-        return "/"
-    }
-
     if ('page_thematique' === document.type) {
         const thematicUid = document.uid;
         return `/${thematicUid}`;
     }
 
     if ('page_article' === document.type) {
-    //     const thematicUid = document.data.thematic.uid;
-        return `/[thematicUid]/${document.uid}`;
+        const thematicUid = document.data.thematic.uid;
+        return `/${thematicUid}/${document.uid}`;
     }
 
     if ('events' === document.type) {
@@ -24,6 +20,9 @@ const linkResolver: LinkResolverFunction = (document: any): string|null => {
         return `/agenda/${document.uid}`;
     }
 
+    if ('homepage' === document.type) {
+        return "/"
+    }
     return null
 }
 

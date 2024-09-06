f8cfffb08a751f70a7d851d086e40e462a592abf
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-09-06 16:57:28 +0200

    Fix responsive agenda view

--- pages/agenda/[uid].vue
+++ pages/agenda/[uid].vue
@@ -80,34 +80,32 @@ useSeo({
                   />
                 </Fancybox>
 
-                <button
-                  type="button"
-                  class="justify-center px-3 py-2.5 text-md font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1"
-                >
-                  <Icon size="18" name="material-symbols:calendar-clock" />
-                  <span v-if="data.startDate">&nbsp;{{ data.startDate }}</span><span v-if="data.endDate"> au {{ data.endDate }}</span>
-                </button>
+                <div class="md:flex-row">
+                  <button
+                    type="button"
+                    class="justify-center px-3 py-2.5 text-md font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1  w-full md:w-auto"
+                  >
+                    <Icon size="18" name="material-symbols:calendar-clock" />
+                    <span v-if="data.startDate">&nbsp;{{ data.startDate }}</span><span v-if="data.endDate"> au {{ data.endDate }}</span>
+                  </button>
 
-                <button
-                  type="button"
-                  class="justify-center px-3 py-2.5 text-2sm font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1"
-                >
-                  <Icon size="18" name="hugeicons:image-composition" />
-                  &nbsp;{{ data.event.data.place_event_txt }}
-                </button>
+                  <button
+                      type="button"
+                      class="justify-center px-3 py-2.5 text-2sm font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1  w-full md:w-auto"
+                  >
+                    <Icon size="18" name="hugeicons:image-composition" />
+                    &nbsp;{{ data.event.data.place_event_txt }}
+                  </button>
 
-                <prismic-link
-                  :field="data.event.data.link"
-                  class="justify-center px-3 py-2.5 text-2sm font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1"
-                  :aria-label="t('layout.moreInfo')"
-                >
-                  {{ $t('layout.moreInfo') }}
-                  <Icon name="material-symbols:arrow-right-alt" />
-                </prismic-link>
-
-                <AddToCalendarButton
-                  :name="data.event.data.title"
-                />
+                  <prismic-link
+                      :field="data.event.data.link"
+                      class="justify-center px-3 py-2.5 text-2sm font-medium text-white inline-flex items-center bg-gray-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center m-1  w-full  md:w-auto"
+                      :aria-label="t('layout.moreInfo')"
+                  >
+                    {{ $t('layout.moreInfo') }}
+                    <Icon name="material-symbols:arrow-right-alt" />
+                  </prismic-link>
+                </div>
 
               </div>
             </div>
